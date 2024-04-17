f1567394480585471891ba178f63e0c1a111c5e0
Jonathan Turck <jonathanturck@tamu.edu>
2024-04-17 12:33:28 -0500

    added import command

---
+++ 16S_preprocessing-2024.sh
@@ -0,0 +1,12 @@
+# This command list handles the initial importing and preprocessing of the data
+# A manifest file must be created to handle this
+# $case-sensitive input/output, please specify specific file name, path, or depth
+
+# create manifest w/ CreateManifest.sh
+
+qiime tools import \
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path $manifest \
+  --input-format PairedEndFastqManifestPhred33V2 \
+  --output-path demux-paired-end.qza
+
